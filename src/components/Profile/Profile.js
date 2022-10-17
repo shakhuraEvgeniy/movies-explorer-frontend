31c24d6664afff01dba29b581776bca36111c098
Evgeniy Shakhura <shah5@yandex.ru>
2022-10-17 22:07:33 +0300

    feat: profile add button

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../Header/Header";
 import "./Profile.css"
 
 const Profile = ({name="Виталий", email="pochta@yandex.ru"}) => {
+  const [edit, setEdit] = useState(false)
+
+  const onEditProfile = () => {
+    setEdit(true);
+  }
+
+
   return (
     <section className="profile">
       <Header />
@@ -10,18 +17,18 @@ const Profile = ({name="Виталий", email="pochta@yandex.ru"}) => {
       <form className="profile__form">
         <div className="profile__input-block">
           <h3 className="profile__name">Имя</h3>
-          <input placeholder={name} className="profile__input"></input>
+          <input value={name} className="profile__input" disabled={!edit}></input>
           <span className="profile-name-input-error profile__input-error"></span>
         </div>
         <div className="profile__input-block">
           <h3 className="profile__name">E-mail</h3>
-          <input placeholder={email} className="profile__input"></input>
+          <input value={email} className="profile__input" disabled={!edit}></input>
           <span className="profile-name-input-error profile__input-error"></span>
         </div>
-        <button className="profile__edit">Редактировать</button>
-        <button className="profile__logout">Выйти из аккаунта</button>
+        <button type="submit" className={`profile__submit ${edit || "button_disabled"}`}>Сохранить</button>
       </form>
-
+      <button className={`profile__edit ${edit && "button_disabled"}`} onClick={onEditProfile}>Редактировать</button>
+      <button className={`profile__logout ${edit && "button_disabled"}`}>Выйти из аккаунта</button>
     </section>
   );
 };
