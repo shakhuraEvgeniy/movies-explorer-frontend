c4f5b558b2f87ea4eebd28621d70bded211f96b9
Evgeniy Shakhura <shah5@yandex.ru>
2022-11-14 15:27:32 +0300

    fix: add validation email

--- src/components/hooks/useFormWithValidation.js
+++ src/components/hooks/useFormWithValidation.js
@@ -1,6 +1,7 @@
-import { useCallback, useState } from "react"
+import { useCallback, useState } from "react";
+import { validate } from "react-email-validator";
 
-export const useFormWithValidation = (inputValues) =>{
+export const useFormWithValidation = (inputValues) => {
   const [values, setValues] = useState(inputValues);
   const [errors, setErrors] = useState({});
   const [isValid, setIsValid] = useState(false);
@@ -9,9 +10,26 @@ export const useFormWithValidation = (inputValues) =>{
     const target = event.target;
     const name = target.name;
     const value = target.value;
-    setValues({...values, [name]: value});
-    setErrors({...errors, [name]: target.validationMessage });
-    setIsValid(target.closest("form").checkValidity());
+
+    setValues({ ...values, [name]: value });
+
+    if (name === "emailInput") {
+      if (value) {
+        if (validate(value)) {
+          setIsValid(true);
+          setErrors({ ...errors, [name]: "" });
+        } else {
+          setErrors({ ...errors, [name]: "Введен некорректный email" });
+          setIsValid(false);
+        }
+      } else {
+        setErrors({ ...errors, [name]: "Заполните это поле" });
+        setIsValid(false);
+      }
+    } else {
+      setIsValid(target.closest("form").checkValidity());
+      setErrors({ ...errors, [name]: target.validationMessage });
+    }
   };
 
   const resetForm = useCallback(
@@ -24,4 +42,4 @@ export const useFormWithValidation = (inputValues) =>{
   );
 
   return { values, handleChange, setValues, errors, isValid, resetForm };
-}
+};
